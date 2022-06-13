1c44900f1e247d3fe0ee3a55c84862a091ec5cfd
zexter <zexter@umich.edu>
2022-06-12 21:54:04 -0400

    Inital push

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+//import profile pic from assets folder
+import myFace from "../assets/myFace.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
-      </div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              src={myFace}
+              alt="profile"
+              class="profile_image"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Joshua Zexter</div>
+            <div className="brief_description">
+              I'm a rising sophmore at the University of Michigan studying Computer Science. I love to swim and play poker!
+            </div>
+          </div>
+        </div>
+     </div>
     )
   }
 }
